5a705b004aa9614c5dac8aba074eb964dbcb1d13
Fede-Arevalo <arevalo.feder@gmail.com>
2022-12-27 12:40:24 +0100

    mejoras CSS

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 const peliculasDiv = document.querySelector(".peliculas");
 const formBuscar = document.getElementById("form");
 
@@ -6,16 +5,14 @@ const mostrarPelicula = (pelicula) => {
   peliculasDiv.innerHTML = "";
   pelicula.forEach((pelicula) => {
     peliculasDiv.innerHTML += `
-                  <div class="card col-lg-3 col-xs-12 col-md-6 m-1">
-                      <div class="personaje">
+                  <div class="card m-1" style="width: 18rem;">
+                    <img src="https://image.tmdb.org/t/p/w300_and_h450_bestv2/${pelicula.poster_path}" class="card-img-top" alt="Card image">
                       <div class="card-body">
-                      <h3 class="card-header">${pelicula.title}</h3>
-                      <h5 class="card-title">${pelicula.overview}</h5>
-                      <img style="height: 200px; width: 100%; display: block;" src="https://image.tmdb.org/t/p/w300_and_h450_bestv2/${pelicula.poster_path}"  alt="Card image">
+                        <h4 class="card-title">${pelicula.title}</h4>
+                          <p class="card-text">${pelicula.overview}</p>
                       </div>
-                      </div>
-                      </div>
-                       `;
+                  </div> 
+    `;
   });
 };
 
@@ -23,7 +20,9 @@ const buscarPelicula = async (e) => {
   e.preventDefault();
   try {
     const busqueda = e.target.buscar.value;
-    const res = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=942ad2a0cd0e42b4e326e96febbc6d53&language=en-US&query=${busqueda}&page=1&include_adult=false`);
+    const res = await axios.get(
+      `https://api.themoviedb.org/3/search/movie?api_key=942ad2a0cd0e42b4e326e96febbc6d53&language=en-US&query=${busqueda}&page=1&include_adult=false`
+    );
     const pelicula = res.data.results;
     mostrarPelicula(pelicula);
   } catch (error) {
